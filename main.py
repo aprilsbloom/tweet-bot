029e1eb2d37ef547b1e4c36b7dbd293dfadcc2cf
paintingofblue <90877067+aprilsbloom@users.noreply.github.com>
2024-10-31 22:45:11 +1300

    Update main.py

--- main.py
+++ main.py
@@ -5,41 +5,46 @@ from pathlib import Path
 from typing import Union
 from discord.ext import commands
 
+
 class Bot(commands.Bot):
-	def __init__(self):
-		super().__init__(intents = discord.Intents.default(), command_prefix='')
+  def __init__(self):
+    super().__init__(intents=discord.Intents.none(), command_prefix="")
 
-	async def setup_hook(self):
-		await self.setupCommands("cogs")
+  async def setup_hook(self):
+    await self.setupCommands("cogs")
 
-		try:
-			synced = await bot.tree.sync()
-			print(f"Synced {len(synced)} commands")
-		except Exception:
-			print(f"An error has occurred while syncing commands.\n{traceback.format_exc()}")
+    try:
+      synced = await bot.tree.sync()
+      print(f"Synced {len(synced)} commands")
+    except Exception:
+      print(
+        f"An error has occurred while syncing commands.\n{traceback.format_exc()}"
+      )
 
-	async def setupCommands(self, directory: Union[str, os.PathLike, Path] = "cogs"):
-		for root, _, files in os.walk(directory):
-			for file in files:
-				if file.endswith(".py") and not file.startswith("_"):
-					cog_path = (os.path.join(root, file).replace(os.sep, ".").rstrip(".py"))
+  async def setupCommands(self, directory: Union[str, os.PathLike, Path] = "cogs"):
+    for root, _, files in os.walk(directory):
+      for file in files:
+        if not (file.endswith(".py") or file.startswith("_")):
+          continue
 
-					try:
-						await self.load_extension(cog_path)
-					except (commands.errors.ExtensionAlreadyLoaded):
-						pass
-					except Exception:
-						print(f'Unable to load {cog_path}\n{traceback.format_exc()}')
+        cog_path = os.path.join(root, file).replace(os.sep, ".").rstrip(".py")
 
-	async def on_ready(self):
-		await self.wait_until_ready()
-		print(f"Logged in as {self.user}.")
+        try:
+          await self.load_extension(cog_path)
+        except commands.errors.ExtensionAlreadyLoaded:
+          pass
+        except Exception:
+          print(f"Unable to load {cog_path}\n{traceback.format_exc()}")
 
-		# current_time = datetime.now()
-		# goal_timestamp = current_time + timedelta(hours = 1, minutes = -current_time.minute)
-		# cfg.set('next_post_time', int(goal_timestamp.timestamp()))
-		# log.info('Starting loop at ' + goal_timestamp.strftime('%H:%M:%S'))
-		# await asyncio.sleep((goal_timestamp - current_time).total_seconds())
+  async def on_ready(self):
+    await self.wait_until_ready()
+    print(f"Logged in as {self.user}.")
+
+    # current_time = datetime.now()
+    # goal_timestamp = current_time + timedelta(hours = 1, minutes = -current_time.minute)
+    # cfg.set('next_post_time', int(goal_timestamp.timestamp()))
+    # log.info('Starting loop at ' + goal_timestamp.strftime('%H:%M:%S'))
+    # await asyncio.sleep((goal_timestamp - current_time).total_seconds())
 
 
 token = ""
